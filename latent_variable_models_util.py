9ecc17dd28d2b099b93490ab07986cf29f5d09b9
Martin Krasser <krasserm@googlemail.com>
2020-04-16 07:14:48 +0200

    Add colors parameter to plot_densities function

--- latent_variable_models_util.py
+++ latent_variable_models_util.py
@@ -37,13 +37,13 @@ def plot_data(X, color='grey', alpha=0.7):
     plt.scatter(X[:,0], X[:,1], c=color, alpha=alpha)
 
 
-def plot_densities(X, mu, sigma, alpha=0.5):
+def plot_densities(X, mu, sigma, alpha=0.5, colors='grey'):
     grid_x, grid_y = np.mgrid[X[:,0].min():X[:,0].max():200j,
                      X[:,1].min():X[:,1].max():200j]
     grid = np.stack([grid_x, grid_y], axis=-1)
 
     for mu_c, sigma_c in zip(mu, sigma):
-        plt.contour(grid_x, grid_y, mvn(mu_c, sigma_c).pdf(grid), colors='grey', alpha=alpha)
+        plt.contour(grid_x, grid_y, mvn(mu_c, sigma_c).pdf(grid), colors=colors, alpha=alpha)
 
 
 def plot_gmm_plate(filename="gmm.png", dpi=100):
