192ef0ec7dc8a6e58985b1601fe2c91263827193
Onur Boyar <boyaronur@gmail.com>
2023-02-01 11:52:41 +0900

    dsf

--- autoencoder-applications/run.sh
+++ autoencoder-applications/run.sh
@@ -1,8 +1,8 @@
 
-checkpoints=(  "checkpoints/model_alpha_1_beta_1_phi_0.pth" "checkpoints/model_alpha_1_beta_1_phi_1.pth"
-                "checkpoints/model_alpha_1_beta_1_phi_2.pth" "checkpoints/model_alpha_1_beta_1_phi_5.pth"
-                "checkpoints/model_alpha_1_beta_1_phi_10.pth" "checkpoints/model_alpha_1_beta_1_phi_15.pth"
-                "checkpoints/model_alpha_1_beta_1_phi_20.pth")
+checkpoints=(  "results/checkpoints/model_alpha_1_beta_1_phi_0.pth" "results/checkpoints/model_alpha_1_beta_1_phi_1.pth"
+                "results/checkpoints/model_alpha_1_beta_1_phi_2.pth" "results/checkpoints/model_alpha_1_beta_1_phi_5.pth"
+                "results/checkpoints/model_alpha_1_beta_1_phi_10.pth" "results/checkpoints/model_alpha_1_beta_1_phi_15.pth"
+                "results/checkpoints/model_alpha_1_beta_1_phi_20.pth")
 
 for ckpt in "${checkpoints[@]}"; do
     python results_eval.py "$ckpt"
